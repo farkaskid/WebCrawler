fde3f1436a509ed94396cfa3cce6d8e56bed6a4d
farkaskid <sid.shishu@gmail.com>
2018-07-11 00:56:24 +0530

    Code formatting changes.

--- crawler/processor.go
+++ crawler/processor.go
@@ -6,28 +6,36 @@ import (
 	"net/http"
 )
 
+// CrawlReport represents the results of the crawling task of a single URL.
 type CrawlReport struct {
 	URL           string
-	HttpStatus    int
+	HTTPStatus    int
 	Err           string
 	ConnectedURLs []string
 }
 
+// Status returns the HTTPStatus of the GET request on the URL of this report.
 func (report CrawlReport) Status() int {
-	return report.HttpStatus
+	return report.HTTPStatus
 }
 
 func (report CrawlReport) String() string {
-	if report.HttpStatus == 0 {
+	if report.HTTPStatus == 0 {
 		return "Failed to crawl URL: " + report.URL + ". Cause: " + report.Err
 	}
 
-	return fmt.Sprintf("Found %d URLs on %s which responded with %d. ", len(report.ConnectedURLs), report.URL, report.HttpStatus)
+	return fmt.Sprintf("Found %d URLs on %s which responded with %d. ", len(report.ConnectedURLs),
+		report.URL, report.HTTPStatus)
 }
 
+// DefaultProcessor is the default implementation of the crawler.Processor interface. This just
+// creates a appropriate CrawlReport instance from the results of the collector.
 type DefaultProcessor struct{}
 
-func (processor DefaultProcessor) Process(requestedURL string, res *http.Response, connectedURLs []string, err error) executor.Report {
+// Process creates a CrawlReport instance from the given parameters. It should be noted that when
+// http.Response is nil then the HTTPStatus in the CrawlReport is set as 0.
+func (processor DefaultProcessor) Process(requestedURL string, res *http.Response, connectedURLs []string,
+	err error) executor.Report {
 	if res == nil {
 		return CrawlReport{requestedURL, 0, err.Error(), make([]string, 0)}
 	}
